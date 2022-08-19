3a86b98e638eb4a668a8541af09f2f2467b03297
pawgog <pgogola10@gmail.com>
2022-08-19 16:52:24 +0200

    Set up react and prettier environments

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    plugins: ['@typescript-eslint'],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    env: {
+        node: true,
+    },
+};
